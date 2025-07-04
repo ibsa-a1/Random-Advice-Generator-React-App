32c55df03406edea05c7e048cf695067c4fbb394
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-04 18:29:39 +0300

    Update basic jsx

--- random-advice/src/App.js
+++ random-advice/src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Random Advice</h1>
-    </div>
-  );
+class App extends Component {
+  state = { advice: ' '}
+
+  fetchadvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response) => {
+      const {advice} = response.data.slip
+      this.setState({advice: advice})
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+  componentDidMount(){
+    this.fetchadvice()
+  }
+
+  render() {
+    const {advice} = this.state
+    return (
+      <div>
+        <h1>{advice}</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
